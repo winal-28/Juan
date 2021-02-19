97f0fd7fe7066b62657c5e3a6926e2c4cd02ce7c
Alwin Villero <winalrollevi@gmail.com>
2021-02-19 13:55:44 +0800

    e

--- juan.py
+++ juan.py
@@ -9,6 +9,7 @@ juan = commands.Bot(command_prefix = 'j ')
 async def on_ready():
     print('Im in.')
     await juan.change_presence(activity=discord.Game(name="j commands")), juan.remove_command('help')
+    
 
 @juan.command()
 async def ping(ctx):
@@ -16,7 +17,7 @@ async def ping(ctx):
 
 @juan.command()
 async def kamusta(ctx):
-    await ctx.send('Kamusta, kababayan! My name is Juan, I am a discord bot from the Philipines! I started being developed at exactly 2/17/2021, by ALwin#6481')
+    await ctx.send('Kamusta, kababayan! My name is Juan, I am a discord bot from the Philipines! I started being developed at exactly 2/17/2021, by ALwin#6481. I am always in development.')
 
 @juan.command()
 async def commands(ctx):
@@ -33,11 +34,11 @@ async def makemefeelbetter(ctx):
 
 @juan.command()
 async def roast_quaccy(ctx):
-    await ctx.send(f'Hey ~~Quaccy~~ Periodiccia, Lets Pight!')
+    await ctx.send(f'No, I dont think I will')
 
 @juan.command()
 async def praise_yourself(ctx):
-    await ctx.send(f'```This is Juan, he is very smart, be like Juan```')
+    await ctx.send(f'```This is Juan, he is very smart, be like Juan.```')
 
 @juan.command()
 async def nice(ctx):
@@ -62,7 +63,7 @@ async def rap(ctx):
 
 @juan.command()
 async def like(ctx):
-    await ctx.send(f'Periodiccia, :flushed::rose:')
+    await ctx.send(f'Periodiccia, :heart::rose:')
 
 @juan.command(aliases = ['adobo', 'adoboe'])
 async def abobo(ctx):
@@ -74,6 +75,38 @@ async def rice(ctx):
     rice_picsandgifs = ['https://media.tenor.com/images/93ceee83581f9c349b567807abed8d98/tenor.gif', 'https://media.tenor.com/images/54abcc48c38f29750afdbab38326ee7c/tenor.gif', 'https://media.tenor.com/images/3a08b4b5ff0c4afdf9a30dc75ffd4daf/tenor.gif', 'https://media.tenor.com/images/92b1bfc8bde13361f0ea6a7a40d6cd22/tenor.gif' ]
     await ctx.send(f'RICE {random.choice(rice_picsandgifs)}')
 
+
+
+@juan.command()
+async def msg(ctx, *, message):
+    try:
+        await ctx.message.delete()
+        await ctx.send(message)
+    except:
+        await ctx.send("Please Give Some Message!")
+
+@juan.command()
+async def invite(ctx):
+    await ctx.send(f'https://discord.com/api/oauth2/authorize?client_id=811521046377005068&permissions=0&scope=bot')
+
+
+@juan.command()
+async def mikeybustos(ctx):
+    embed=discord.Embed(title="Mikey Bustos", url="https://duckduckgo.com/mikeybustos", description="Michael John Tumanguil Pestano Bustos is a Filipino–Canadian singer and comedian who has appeared on the reality television show Canadian Idol. He is also the owner of AntsCanada, an online shop that specialises in ant-keeping.", color=0xFF5733)
+    embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Mikeyredcarpet.jpg/220px-Mikeyredcarpet.jpg")
+    await ctx.send(embed=embed)
+
+@juan.command()
+async def president(ctx):
+    embed=discord.Embed(title="Rodrigo Roa Duterte", url="https://duckduckgo.com/rodrigoduterte", description="Rodrigo Roa Duterte, also known as Digong and Rody, is a Filipino politician who is the current president of the Philippines and the first from Mindanao to hold the office. He is the chairperson of PDP–Laban, the ruling political party in the Philippines.", color=0xFF5733)
+    embed.set_thumbnail(url="https://duckduckgo.com/i/2c883356.jpg")
+    await ctx.send(embed=embed)
+
+@juan.command()
+async def vicepresident(ctx):
+    embed=discord.Embed(title="Ferdinand Marcos Jr.", url="https://duckduckgo.com/rodrigoduterte", description="Rodrigo Roa Duterte, also known as Digong and Rody, is a Filipino politician who is the current president of the Philippines and the first from Mindanao to hold the office. He is the chairperson of PDP–Laban, the ruling political party in the Philippines.", color=0xFF5733)
+    embed.set_thumbnail(url="https://duckduckgo.com/i/2c883356.jpg")
+    await ctx.send(embed=embed)
 
 
 #@juan.event
